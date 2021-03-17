1ba15fc048d086825078f2c490b332c3c78b39bc
lafernandes22 <fernandesalucas2018@gmail.com>
2021-03-17 19:06:58 -0400

    working static map

--- project1app/src/components/finalproject/USAMap.js
+++ project1app/src/components/finalproject/USAMap.js
@@ -22,10 +22,10 @@ class USAMap extends Component {
     let { windowWidth, windowHeight } = this.state; 
     let w = windowWidth / 2;
     let h = windowHeight / 2; 
-    
+
     var projection = d3.geoAlbersUsa()
       .translate([w/2, h/2])    // translate to center of screen
-      .scale([1300]);          // scale things down so see entire US
+      .scale([900]);          // scale things down so see entire US
 
     var svg = d3.select(this.myRef.current)
 			.append("svg")
@@ -35,15 +35,28 @@ class USAMap extends Component {
     var path = d3.geoPath()
       .projection(projection);
     
-    d3.json("data/us-states.json").then(states=> {
-      svg.selectAll("path")
-        .data(states.features)
-        .enter()
-        .append("path")
-        .attr("id", function(d) { return d.properties.name;})
-        .attr("d", path)
-        .style("stroke", "grey")
-        .style("fill", "rgb(213,222,217)");
+    d3.csv("data/reopened_seated_diner_data.csv").then(function(seatData) {
+      console.log(seatData);
+      var color = d3.scaleLinear().domain([0,100]).range(["rgb(213,222,217)", "green"]);
+      d3.json("data/us-states.json").then(states=> {
+        svg.selectAll("path")
+          .data(states.features)
+          .enter()
+          .append("path")
+          .attr("id", function(d) { return d.properties.name;})
+          .attr("d", path)
+          .style("stroke", "grey")
+          .style("fill", function(d) {
+            let colorC = "rgb(213,222,217)";
+            seatData.forEach(function(seats) {
+              if(seats["Name"] === d.properties.name){
+                colorC = color(seats["JUN12020"]);
+              }
+            })
+            return colorC;
+          })
+      });
+
     });
   }
 
@@ -55,6 +68,7 @@ class USAMap extends Component {
     return <div>
               <h4>USA Map Component</h4>
               <div className="mapUSA" ref={this.myRef}></div>  
+              <select id="mapButton"></select>
            </div>
   }
 } 
